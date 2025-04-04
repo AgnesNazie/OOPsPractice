563381da50de9de43e2a0f12d9258822b957729f
AgnesNazie <cheagnesmbo@gmail.com>
2025-04-04 21:18:55 +0200

    practicing polymorphism

---
+++ src/main/java/com/agnes/MathOperation.java
@@ -0,0 +1,14 @@
+package com.agnes;
+
+public class MathOperation {
+    //polymorphism  using method overloading
+    int add (int a, int b){
+        return a + b;
+    }
+    int sub (int a, int b) {
+        return a - b  ;
+    }
+    int mul (int a, int b) {
+        return  a * b ;
+    }
+}
