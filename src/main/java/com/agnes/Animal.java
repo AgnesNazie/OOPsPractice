2ba8b61dbad64a5bccbef1badea26771ef29b561
AgnesNazie <cheagnesmbo@gmail.com>
2025-04-04 21:27:42 +0200

    more exercises on polymorphism

--- src/main/java/com/agnes/Animal.java
+++ src/main/java/com/agnes/Animal.java
@@ -1,4 +1,27 @@
 package com.agnes;
 
 public class Animal {
+    void speak () {
+        System.out.println("Speak");
+    }
+}
+class Dog extends Animal{
+    @Override
+    void speak(){
+        System.out.println("Woof! Woof!");
+    }
+}
+class Cat extends Animal{
+    @Override
+    void speak(){
+        System.out.println("Meow! Meow!");
+    }
+}
+ class AnimalTest{
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+        myCat.speak();
+        myDog.speak();
+    }
 }
