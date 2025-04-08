0885e284ad76cce2363c970be67d8c542088dbcd
AgnesNazie <cheagnesmbo@gmail.com>
2025-04-08 16:50:52 +0200

    created class for car

---
+++ src/main/java/com/agnes/CarCar.java
@@ -0,0 +1,12 @@
+package com.agnes;
+
+public class CarCar {
+    //fields
+    String brand;
+    int speed;
+    //methods
+    void drive(){
+        System.out.println("The " + brand + " is driving at " + speed + " km/h");
+    }
+
+}
